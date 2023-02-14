2ec2fdf371483b14eccdc0dbd1c7353991049b6b
Антон Належитый <a.o.nalezhytyy@student.khai.edu>
2023-02-14 10:57:38 +0200

    Remake task 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,13 @@
 fun main() {
    println("Введите начальный х ")
-   val x_start = readLine()!!.toInt()
+   var x_start = readLine()!!.toInt()
    println("Введите конечный х ")
-   val x_end = readLine()!!.toInt()
+   var x_end = readLine()!!.toInt()
    val a = (x_start..x_end).random()
-   println("Значение а = $a")
+   if (x_end > x_start)
+       x_end = x_start.also { x_start = x_end}
+   println("""Значение а = $a
+       |Изменённый x_start = $x_start
+       |Изменённый x_end = $x_end
+   """.trimMargin())
 }
