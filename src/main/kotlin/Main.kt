94ca4f92b54ff29c8c5909f99c8b25bdfe309df4
Антон Належитый <a.o.nalezhytyy@student.khai.edu>
2023-02-14 10:02:55 +0200

    Task 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,8 @@
 fun main() {
-   var x_start = 0
-   var x_end = 250
-   val a = (x_start.. x_end).random()
-   if (x_end > x_start)
-       x_end = x_start.also { x_start = x_end }
+   val x_start: Double = 0.0
+   val x_end: Double = 250.0
+   val Step = (x_end - x_start)/100
    println(x_start)
    println(x_end)
-   println(a)
+   println(Step)
 }
