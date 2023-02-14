9f7a71ca73f57432e4764bc55f4f7c96ea67414f
Антон Належитый <a.o.nalezhytyy@student.khai.edu>
2023-02-14 10:18:01 +0200

    Task 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,15 @@
+import kotlin.math.*
+import kotlin.random.Random
+
 fun main() {
-   val x_start: Double = 0.0
-   val x_end: Double = 250.0
-   val Step = (x_end - x_start)/100
+   val x_start = 0.0
+   val x_end = 200.0
+   val a = Random.nextDouble(x_start, x_end)
+   val x = Random.nextDouble(x_start, x_end)
+   val y = (x+5.0/(3*x/a)+ln(abs(cos(x)))/2/exp(x)+1.0e-5*x.pow(3))
    println(x_start)
    println(x_end)
-   println(Step)
+   println(a)
+   println(x)
+   println(y)
 }
