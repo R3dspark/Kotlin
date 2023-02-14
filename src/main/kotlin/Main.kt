49a4a500f955a3e316a5d3795e0a1d375cfaecc5
Антон Належитый <a.o.nalezhytyy@student.khai.edu>
2023-02-14 10:50:53 +0200

    Remake task 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,8 @@
-import kotlin.math.*
-import kotlin.random.Random
-
 fun main() {
-   val x_start = 0.0
-   val x_end = 200.0
-   val a = Random.nextDouble(x_start, x_end)
-   val x = Random.nextDouble(x_start, x_end)
-   val y = (x+5.0/(3*x/a)+ln(abs(cos(x)))/2/exp(x)+1.0e-5*x.pow(3))
-   println(x_start)
-   println(x_end)
-   println(a)
-   println(x)
-   println(y)
+   println("Введите начальный х ")
+   val x_start = readLine()!!.toInt()
+   println("Введите конечный х ")
+   val x_end = readLine()!!.toInt()
+   val a = (x_start..x_end).random()
+   println("Значение а = $a")
 }
