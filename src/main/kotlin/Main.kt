c7315cc4bdc671db3febe5dad834b4751a04710a
Антон Належитый <a.o.nalezhytyy@student.khai.edu>
2023-02-14 09:49:24 +0200

    Task 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 fun main() {
-   val x_start = 0
-   val x_end = 250
+   var x_start = 0
+   var x_end = 250
    val a = (x_start.. x_end).random()
+   if (x_end > x_start)
+       x_end = x_start.also { x_start = x_end }
    println(x_start)
    println(x_end)
    println(a)
