687d61bc282aac8c83dff0975605c27532340a11
Антон Належитый <a.o.nalezhytyy@student.khai.edu>
2023-02-21 09:30:30 +0200

    Remake task 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,10 +11,18 @@ fun main() {
     val a = Random.nextDouble(x_start, x_end)
     val diff = (x_end - x_start)
     val Step =  diff/100
+    val numSteps = (diff/Step).toInt() + 1
     if (x_end > x_start)
         x_end = x_start.also { x_start = x_end}
-println("Значение а = " + String.format("%.3f", a))
-println("""Изменённый x_start = $x_start
+    for (i in 0 until numSteps){
+        val x = x_start + i * Step
+        val y = (x+5.0/(3*x/a)+ln(abs(cos(x)))/2/exp(x)+1.0e-5*x.pow(3))
+        println("Значение а = " + String.format("%.3f", a))
+        println("""Изменённый x_start = $x_start
 Изменённый x_end = $x_end
-Шаг = $Step""")}
-
+Шаг = $Step
+Значение у = 
+   """.trimMargin()+ String.format("%.3f", y))
+        break
+    }
+    }
