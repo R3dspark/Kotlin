698467f4b67ebf183d22794d8c22615c3040406e
Антон Належитый <a.o.nalezhytyy@student.khai.edu>
2023-02-21 09:24:31 +0200

    Remake task 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,20 @@
+import kotlin.random.Random
+import kotlin.math.*
+
+
 fun main() {
    println("Введите начальный х ")
-   var x_start = readLine()!!.toInt()
+   var x_start = readLine()!!.toDouble()
    println("Введите конечный х ")
-   var x_end = readLine()!!.toInt()
-   val a = (x_start..x_end).random()
-   if (x_end > x_start)
-       x_end = x_start.also { x_start = x_end}
-   println("""Значение а = $a
-       |Изменённый x_start = $x_start
-       |Изменённый x_end = $x_end
-   """.trimMargin())
-}
+   var x_end = readLine()!!.toDouble()
+
+    val a = Random.nextDouble(x_start, x_end)
+    val diff = (x_end - x_start)
+    val Step =  diff/100
+    if (x_end > x_start)
+        x_end = x_start.also { x_start = x_end}
+println("Значение а = " + String.format("%.3f", a))
+println("""Изменённый x_start = $x_start
+Изменённый x_end = $x_end
+Шаг = $Step""")}
+
